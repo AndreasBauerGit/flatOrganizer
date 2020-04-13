b9781232c01dc1fdf9e340e38fb20bfb764ea216
Andreas Bauer <Andreas.Bauer2@gmx.net>
2020-04-13 18:16:06 +0200

    uploading java project

---
+++ app/src/main/java/com/example/flat_organizer/ui/main/F1.java
@@ -0,0 +1,80 @@
+package com.example.flat_organizer.ui.main;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.example.flat_organizer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link F1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class F1 extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public F1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static F1 newInstance(String param1, String param2) {
+        F1 fragment = new F1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.f1, container, false);
+        Button button = (Button) view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("print1","print1");
+
+            }
+        });
+
+
+        return view;
+    }
+}
